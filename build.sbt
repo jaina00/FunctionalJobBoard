d335fac4247039fdaead0eea175bde8855a027ce
jaina00 <aa.jain04@gmail.com>
2018-03-14 13:34:25 +0000

    commit

--- build.sbt
+++ build.sbt
@@ -5,7 +5,14 @@ version := "0.1"
 scalaVersion := "2.12.4"
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats" % "0.9.0"
+  "org.typelevel" %% "cats" % "0.9.0",
+  "com.chuusai" %% "shapeless" % "2.3.3"
+)
+
+
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("releases"),
+  Resolver.sonatypeRepo("snapshots")
 )
 
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
---
+++ src/main/scala/shapeless/Shapeless.scala
@@ -0,0 +1,22 @@
+package shapeless
+import shapeless.Generic
+import shapeless.syntax.singleton._
+
+
+object Shapeless extends App {
+
+  case class Vec(x: Int, y: Int)
+  case class Rect(origin: Vec, size: Vec)
+
+  def getRepr[A](value: A)(implicit gen: Generic[A]): gen.Repr =
+    gen.to(value)
+
+  val t = getRepr(Vec(1, 2))
+
+  val x = getRepr(Rect(Vec(0, 0), Vec(5, 5)))
+
+
+  var z = 42.narrow
+
+
+}
